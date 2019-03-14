d485c0c7b2a1ddb9d760e31e655a86a3a64bb9ff
mahendriksen <47648898+mahendriksen@users.noreply.github.com>
2019-03-14 14:53:59 +1100

    Additional input method included

--- distancechecker.py
+++ distancechecker.py
@@ -59,29 +59,42 @@ if p == 0:
 	print "Degenerate percentage was %f %%" % (ping_count/m * 100)
 if p == 1:
 	ping_count = 0
-	file = open("testtree1.txt", "r") 
-	treeone = make_tuple(file.read())
-	file.close()
-	file = open("testtree2.txt", "r") 
-	treetwo = make_tuple(file.read())
-	file.close()
-	
-	m = leaf_count(treeone)
-	n = leaf_count(treetwo)
-	
-	if m == n:
-		X = range(1,m+1)
-		one_f =	dirty_calculate_f(treeone)
-		two_f = dirty_calculate_f(treetwo)
-		a = multihierarchy(treeone,treetwo)
-		halfway_point = big_tree(a,ping_count) 
-		ping_count = halfway_point[1]
-		halfway_point = halfway_point[0]
-		halfway_f = clean_calculate_f(halfway_point)
-	else:
-		print "ERROR: Trees have different number of leaves"
-		sys.exit()
 
+	firstfile = open(raw_input("What is the first filename? "), "r") 
+	first_list = firstfile.read().splitlines()
+	firstfile.close()
+	secondfile = open(raw_input("What is the second filename? "), "r") 
+	second_list = secondfile.read().splitlines()
+	secondfile.close()
+	n = input("How many leaves in each tree? ")
+
+	start_time = time.time()
+	distance_data = [0]*(n**2-3*n+2)
+	diameter = 0
+	running_total = 0
+	iterator = 0
+
+	f = len(first_list)
+	s = len(second_list)
+
+	m = f*s
+
+	for  i in xrange(f):
+		for j in xrange(s):
+			iterator += 1
+			treeone = make_tuple(first_list[i])
+			treetwo = make_tuple(second_list[j])
+			ping_count,running_total,diameter = calculation_time(treeone,treetwo,ping_count,running_total,diameter)
+			sys.stdout.write("\rCalculating iteration %i of %i . That's %f %%" % (iterator,m, iterator/m *100))
+			sys.stdout.flush()
+
+	x = time.time() - start_time		
+	print "\nTotal time is ", "--- ", x, " seconds ---" 
+	print "Average time was ", x/m
+	print "Average distance was ", running_total/m
+	print "Diameter was ", diameter
+	print "Degenerate percentage was %f %%" % (ping_count/m * 100)
+	
 with open('%ileafdata.txt' % n, 'w+') as file:
     for number in distance_data:
         file.write("%i,\n" % number)
